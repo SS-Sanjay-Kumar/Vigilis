55ddaf89dc3b588e7700f3156517fb1c631f6fe5
Sanjay Kumar SS <sanjay.savitha05@gmail.com>
2026-08-22 15:04:53 +0530

    chore: go,add comments for better readability

--- internal/hub/anomaly_detection_hub.go
+++ internal/hub/anomaly_detection_hub.go
@@ -5,21 +5,26 @@ import (
 )
 
 type AnomalyDetectionHub struct {
-	mu          sync.RWMutex
+	mu sync.RWMutex
+	//sync.RWMutex allows multiple concurrent readers or a single writer at any given time.
 	subscribers map[chan string]bool
+	// map[T]bool is a standard golang idiom for building a Set of unique items.
+	// this is because golang does not have a native, built-in Set data type
+	//* so subscribers here is just a Set of channels
 }
-
-// subscriber is a hashmap with key = channel with values of type string and
-// value = bool (true indicates that the channel/subscriber is active)
 
 func NewAnomalyDetectionHub() *AnomalyDetectionHub {
 	return &AnomalyDetectionHub{
 		subscribers: make(map[chan string]bool),
+		// in golang maps must be initialized before use, or writing to them causes a runtime panic.
+		// Analogy: we are just creating a Set here called subscribers
 	}
 }
 
 func (h *AnomalyDetectionHub) Subscribe() chan string {
 	h.mu.Lock()
+	// Lock() = no write and no read
+	//* i.e blocks both read and write
 	defer h.mu.Unlock()
 
 	ch := make(chan string, 100) //! temp: buffer capacity is set to 100
@@ -41,7 +46,11 @@ func (h *AnomalyDetectionHub) Unsubscribe(ch chan string) {
 func (h *AnomalyDetectionHub) Broadcast(msg string) {
 	h.mu.RLock()
 	defer h.mu.RUnlock()
-
+	// RLock() =  read lock
+	// multiple concurrent calls to Broadcast can read from h.subscribers simultaneously, 
+	// but no writer can alter the subscribers list during execution.
+	//* i.e blocks only write
+	
 	for ch := range h.subscribers {
 		select {
 		case ch <- msg:
--- internal/middleware/metrics.go
+++ internal/middleware/metrics.go
@@ -7,11 +7,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// PrometheusMiddleware() is a factory function, i.e function that returns another function
 func PrometheusMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Next() // Process request
 
-		status := strconv.Itoa(c.Writer.Status())
+		status := strconv.Itoa(c.Writer.Status()) //converts integer to string
 		metrics.HttpRequestsTotal.WithLabelValues(c.Request.Method, c.FullPath(), status).Inc()
 	}
 }
